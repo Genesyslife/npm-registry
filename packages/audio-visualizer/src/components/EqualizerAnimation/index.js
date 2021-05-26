e3b343aaf05f89c0d2d01fa17cbebedde817bd45
guigallo <guilherme.gg1@hotmail.com>
2021-05-26 18:44:23 -0300

    export track slider for video

---
+++ packages/audio-visualizer/src/components/EqualizerAnimation/index.js
@@ -0,0 +1,10 @@
+// import React, { useMemo } from "react";
+import styles from "./EqualizerAnimation.module.scss";
+
+export default function EqualizerAnimation() {
+  // const bars = useMemo(() => Array.from({length: barsCount}, (x, i) => i), [
+  //   barsCount,
+  // ])
+
+  return <div className={styles["equalizer"]}></div>;
+}
