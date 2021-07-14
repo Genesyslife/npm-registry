b2770989994dfb8bf68abda65164eabaef66f8da
Guilherme <guilherme.gg1@hotmail.com>
2021-07-14 11:47:47 -0300

    add fps player story

---
+++ docs/stories/FPSPlayer.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Player from '@Genesys/rtf-fps-player'
+
+export default {
+  component: Player,
+  title: '3D/FPS Player',
+}
+
+export const Default = () => <Player />
